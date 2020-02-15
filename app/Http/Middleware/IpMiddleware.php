bf2378c535946d5089c5eee094a43cc1e63843a3
Shahid Umar <shahid.umar@gmail.com>
2020-02-15 17:39:57 +0500

    IP Middle ware ON

--- app/Http/Middleware/IpMiddleware.php
+++ app/Http/Middleware/IpMiddleware.php
@@ -20,13 +20,13 @@ class IpMiddleware
         FIBERLINKS2	27.255.4.50    
         FORTUNES 	124.109.48.193 
         Comsats     203.124.41.82   */
-        // $whitelistedip=['27.255.4.203', '27.255.4.50', '124.109.48.193', '203.124.41.82'];
+         $whitelistedip=['27.255.4.203', '27.255.4.50', '124.109.48.193', '203.124.41.82'];
 
-        // if (!in_array($request->ip(), $whitelistedip) && auth()->user()->role_id!='1') {
-        //     // here instead of checking a single ip address we can do collection of ips
-        //     //address in constant file and check with in_array function            
-        //     return redirect('home');
-        // }
+         if (!in_array($request->ip(), $whitelistedip) && auth()->user()->role_id!='1') {
+             // here instead of checking a single ip address we can do collection of ips
+             //address in constant file and check with in_array function            
+             return redirect('home');
+         }
         return $next($request);
     }
 
